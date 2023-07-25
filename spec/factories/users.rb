4b3751afb25e3f23add684691df1e68821843a94
chibicatt2 <s.taitai0725@gmail.com>
2023-07-26 06:08:44 +0900

    モデル単体テストコード記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.initials}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    family_name {Faker::Name.last_name}
-    first_name {Faker::Name.first_name}
-    family_name_kana {Faker::Name.initials}
-    first_name_kana {Faker::Name.initials}
-    date_birth {Faker::Date.birthday}
+    family_name           {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+    family_name_kana      {Faker::Name.initials(number: 2)}
+    first_name_kana       {Faker::Name.initials(number: 2)}
+    date_birth            {Faker::Date.birthday}
   end
 end
