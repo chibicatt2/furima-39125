d62441a0fb9241abd7bc826a980a1300ba574894
chibicatt2 <s.taitai0725@gmail.com>
2023-07-26 21:29:44 +0900

    正規表現を用いたバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英数字の両方を含めて設定してください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角（カタカナ）を使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  
+
   validates :date_birth, presence: true
 end
