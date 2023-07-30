caee582ba1f3566674e80cd4f533b6204b1ffb38
chibicatt2 <s.taitai0725@gmail.com>
2023-07-30 16:21:33 +0900

    コントローラーの定義（createアクション）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :time_id, :price).merge(user_id: current_user.id)
+  end
 end
