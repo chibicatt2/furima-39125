dbf335c93765246a4f6b460c644488484f5093e4
chibicatt2 <s.taitai0725@gmail.com>
2023-07-30 12:11:42 +0900

    ActiveHashのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :area
+  belongs_to :time
   belongs_to :user
   has_one_attached :image
+  
 end
