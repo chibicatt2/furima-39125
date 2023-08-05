44972522a17ab2b1b55b32f71c872bde69ad0442
chibicatt2 <s.taitai0725@gmail.com>
2023-08-05 20:34:02 +0900

    マイグレーションファイル編集、テーブル作成

--- db/migrate/20230805111701_create_deliveries.rb
+++ db/migrate/20230805111701_create_deliveries.rb
@@ -1,6 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
+      t.string :postcode, null: false
+      t.integer :area_id, null: false
+      t.string :municipality, null: false
+      t.string :address, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.references :order, null: false, foreign_key: true
 
       t.timestamps
     end
