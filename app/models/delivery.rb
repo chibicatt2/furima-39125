367a75ce2a8559a35d89aaae850d46bb3a34b575
chibicatt2 <s.taitai0725@gmail.com>
2023-08-06 14:25:29 +0900

    バリデーション設定

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,14 @@
 class Delivery < ApplicationRecord
   belongs_to :order
+
+  validates :postcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :municipality, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: '10桁以上11桁以内の半角数字で入力してください'}
+  
+  
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :area_id
+  end
+
 end
