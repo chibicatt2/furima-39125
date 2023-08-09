7cc9da17f8ad73571c741444fc2b051d217a04ca
chibicatt2 <s.taitai0725@gmail.com>
2023-08-09 16:34:26 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @order_derivery = OrderDerivery.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
