4b3fa9d7ba408f0588577eaa97e36c16c65cf4af
chibicatt2 <s.taitai0725@gmail.com>
2023-08-24 22:45:33 +0900

    JavaScriptで情報を画面に表示

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,6 +1,8 @@
 class CommentChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "comment_channel"
+    @item = Item.find(params[:item_id])
+    stream_for @item
+
   end
 
   def unsubscribed
